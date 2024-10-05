f7ea5940aa37e24ecbe07caab809b9cd55a78073
TrungKien02062005 <lekien020605@gmail.com>
2024-10-05 10:19:45 +0700

    commit

---
+++ src/calculation/Main.java
@@ -0,0 +1,11 @@
+
+package calculation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        calculation calculation = new calculation();
+                
+    }
+    
+}
